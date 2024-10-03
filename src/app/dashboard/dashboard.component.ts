dd425f81aa0eaae6463539be3e5dd5d40b9a7286
Prabhat Kumar <kumarprabhat0224@gmail.com>
2024-10-03 08:50:24 +0530

    Add Authentication

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,7 +2,8 @@ import { Component, NgModule, OnInit } from '@angular/core';
 import { ItemService } from "../services/item.service";
 import { item } from '../data-type';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,8 +15,9 @@ import { RouterModule } from '@angular/router';
 export class DashboardComponent implements OnInit {
  itemList : undefined | item[]
  deleteMessage: undefined | string;
+ //isAuthenticated: undefined | boolean;
 
-  constructor(private itemService: ItemService) {}
+  constructor(private itemService: ItemService, private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     this.loadItems();
@@ -41,6 +43,14 @@ export class DashboardComponent implements OnInit {
       this.itemList = result
     })
   }
+  
+  logout(): void{
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 
+  isAuthenticated():boolean{
+    return this.authService.isAuthenticated();
+  }
   
 }
